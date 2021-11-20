3a4d0703975373a28ad18abf0e7ecb676270f2df
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2021-11-20 06:06:48 +0530

    Added bus, mesh, ring, star

---
+++ week3/bus.tcl
@@ -0,0 +1,24 @@
+LanRouter set debug_ 0
+set ns [new Simulator]
+
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+    global ns nf
+    $ns flush-trace
+    close $nf
+    puts "running nam"
+    exec nam out.nam &
+    exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
+$ns newLan "$n0 $n1 $n2 $n3" 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel
+
+$ns at 5.00 "finish"
+$ns run
