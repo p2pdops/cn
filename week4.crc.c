949239175c7fb67ededcea13ee9724f09dd55e10
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2022-01-09 15:34:09 -0800

    Update week4.crc.c

--- week4.crc.c
+++ week4.crc.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
-void main()
+
+char xor (char a, char b)
 {
-    int i, j, keylen, msglen;
-    char input[100], key[30], temp[30], quot[100], rem[30], key1[30];
-    printf("Enter Data: ");
+    if (a == b)
+        return '0';
+    return '1';
+}
+
+    int main()
+{
+    char input[100], bkpinput[100], temp[100], quot[100];
+    char key[30], bkpkey[30], rem[30];
+    int msglen, keylen;
+
+    printf("Enter your data: ");
     gets(input);
-    printf("Enter Key: ");
+    printf("Enter divisor: ");
     gets(key);
-    keylen = strlen(key);
+
     msglen = strlen(input);
-    strcpy(key1, key);
-    for (i = 0; i < keylen - 1; i++)
-    {
+    keylen = strlen(key);
+    strcpy(bkpinput, input);
+    strcpy(bkpkey, key);
+
+    printf("Crc encoded data is : %s\n", input);
+
+    for (int i = 0; i < keylen - 1; i++)
         input[msglen + i] = '0';
-    }
-    for (i = 0; i < keylen; i++)
+
+    for (int i = 0; i < keylen; i++)
         temp[i] = input[i];
-    for (i = 0; i < msglen; i++)
+
+    for (int i = 0; i < msglen; i++)
     {
         quot[i] = temp[0];
-        if (quot[i] == '0')
-            for (j = 0; j < keylen; j++)
-                key[j] = '0';
-        else
-            for (j = 0; j < keylen; j++)
-                key[j] = key1[j];
-        for (j = keylen - 1; j > 0; j--)
-        {
-            if (temp[j] == key[j])
-                rem[j - 1] = '0';
-            else
-                rem[j - 1] = '1';
-        }
+        for (int j = 0; j < keylen; j++)
+            key[j] = (quot[i] == '0') ? '0' : bkpkey[j];
+        for (int j = keylen - 1; j > 0; j--)
+            rem[j - 1] = xor(temp[j], key[j]);
         rem[keylen - 1] = input[i + keylen];
         strcpy(temp, rem);
     }
     strcpy(rem, temp);
-    printf("\nQuotient is ");
-    for (i = 0; i < msglen; i++)
-        printf("%c", quot[i]);
-    printf("\nRemainder is ");
-    for (i = 0; i < keylen - 1; i++)
-        printf("%c", rem[i]);
-    printf("\nFinal data is: ");
-    for (i = 0; i < msglen; i++)
-        printf("%c", input[i]);
-    for (i = 0; i < keylen - 1; i++)
-        printf("%c", rem[i]);
-    getch();
+
+    printf("Quoitent is %s\n", quot);
+    printf("Remainder is %s\n", rem);
+    printf("Data to send is %s%s\n", bkpinput, rem);
+
+    return 0;
 }
