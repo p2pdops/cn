0e269142ceeec90ee1582da74d5a80eadb6efb23
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2022-01-09 16:45:07 -0800

    Update week4.hamming_code.c

--- week4.hamming_code.c
+++ week4.hamming_code.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
-#include <conio.h>
-void main()
+
+int main()
 {
-    int data[7], rec[7], i, c1, c2, c3, c;
-    printf("this works for message of 4bits in size \nenter message bit one by one:  ");
-    scanf("%d%d%d%d", &data[0], &data[1], &data[2], &data[4]);
-    data[6] = data[0] ^ data[2] ^ data[4];
-    data[5] = data[0] ^ data[1] ^ data[4];
-    data[3] = data[0] ^ data[1] ^ data[2];
-    printf("\nthe encoded bits are given below: \n");
-    for (i = 0; i < 7; i++)
-    {
+    int data[8 + 1], rec[8 + 1];
+    printf("Enter 4 bit word: ");
+
+    scanf("%d%d%d%d", &data[3], &data[5], &data[6], &data[7]);
+
+    data[1] = data[3] ^ data[5] ^ data[7];
+    data[2] = data[3] ^ data[6] ^ data[7];
+    data[4] = data[5] ^ data[6] ^ data[7];
+
+    printf("Data to transmit : ");
+    for (int i = 1; i < 7 + 1; i++)
         printf("%d ", data[i]);
-    }
-    printf("\nenter the received data bits one by one: ");
-    for (i = 0; i < 7; i++)
-    {
+
+    printf("\n Enter data received: ");
+
+    for (int i = 1; i < 8; i++)
         scanf("%d", &rec[i]);
-    }
-    c1 = rec[6] ^ rec[4] ^ rec[2] ^ rec[0];
-    c2 = rec[5] ^ rec[4] ^ rec[1] ^ rec[0];
-    c3 = rec[3] ^ rec[2] ^ rec[1] ^ rec[0];
-    c = c3 * 4 + c2 * 2 + c1;
-    if (c == 0)
+
+    int c, c1, c2, c3;
+    c1 = rec[1] ^ rec[3] ^ rec[5] ^ rec[7];
+    c2 = rec[2] ^ rec[3] ^ rec[6] ^ rec[7];
+    c3 = rec[4] ^ rec[5] ^ rec[6] ^ rec[7];
+    c = 4 * c3 + 2 * c2 + c1;
+    if (c)
     {
-        printf("\ncongratulations there is no error: ");
+        printf("Error at position %d \n", c);
+
+        rec[8 - c] = rec[8 - c] ^ 1;
+        printf("Corrected data: ");
+        for (int i = 1; i < 7 + 1; i++)
+            printf("%d ", rec[i]);
     }
     else
     {
-        printf("\nerron on the postion: %d\nthe correct message is \n", c);
-        if (rec[7 - c] == 0)
-            rec[7 - c] = 1;
-        else
-            rec[7 - c] = 0;
-        for (i = 0; i < 7; i++)
-        {
-            printf("%d ", rec[i]);
-        }
+        printf("No error in data!\n");
     }
-    getch();
 }
